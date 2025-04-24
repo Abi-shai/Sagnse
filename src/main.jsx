5f21941cdae8143a445536a46e92f47cc5c910d2
Abishaï Yapélé <abishaiyapele@192.168.1.3>
2025-04-24 18:00:08 +0000

    some modifs in file extension usage

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Site from './Site.jsx'
+import Site from './Site'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
