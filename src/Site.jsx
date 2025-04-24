248bd912aebf20f789f6ebeb9bcd841a024290c4
Abishaï Yapélé <abishaiyapele@192.168.1.3>
2025-04-24 17:24:42 +0000

    More config, coming soon state

---
+++ src/Site.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './Site.css'
+
+function Site() {
+
+  return (
+    <>
+      <p>Coming soon !</p>
+    </>
+  )
+}
+
+export default Site
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import Site from './Site.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Site />
   </StrictMode>
 )
