9e33eda9fd82f5f76c2531e24f0f996ec4e5092c
Abishaï Yapélé <abishaiyapele@192.168.1.5>
2025-05-23 00:21:01 +0000

    camelCase

---
+++ src/app/components/shared/button/button.jsx
@@ -0,0 +1,51 @@
+import { NavLink } from "react-router";
+
+import './button.css';
+
+export const ButtonPrimary = ({ content, size, route, target }) => {
+  return (
+    <NavLink
+      to={route}
+      target={target}
+      className="body2 buttonPrimary"
+      style={
+        size === 'small' ? { padding: '.5em .75em', gap: '.25em' } : { padding: '.75em 1em', gap: '.5em' }
+      }
+    >
+      {content}
+
+    </NavLink>
+  )
+}
+
+
+export const ButtonPrimarySubmit = ({ content, size, typeOfAction }) => {
+  return (
+    <button
+      typeof={typeOfAction}
+      className="body2 buttonPrimary"
+      style={
+        size === 'small' ? { padding: '.5em .75em', gap: '.25em' } : { padding: '.75em 1em', gap: '.5em' }
+      }
+    >
+      {content}
+
+    </button>
+  )
+}
+
+
+
+export const ButtonSecondary = ({ content, size }) => {
+  return (
+    <button
+      className="body2 buttonSecondary"
+      style={
+        size === 'small' ? { padding: '.5em .75em', gap: '.25em' } : { padding: '.75em 1em', gap: '.5em' }
+      }
+    >
+      {content}
+
+    </button>
+  )
+}
